729259c4517cdfa2b804bce8b781f401826ac65f
Jonathan Alvarez <j@jonalvarezz.com>
2022-09-05 22:12:46 +0200

    write main script and create layout
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 import "./style.css";
+import { ethers } from "ethers";
 
-console.log("Hi, Platzi!");
+const process = async () => {
+  const provider = window.ethereum
+    ? new ethers.providers.Web3Provider(window.ethereum)
+    : new ethers.providers.CloudflareProvider();
+
+  const networkInfo = await provider.getNetwork();
+  console.log(networkInfo);
+
+  const blockNumber = await provider.getBlockNumber();
+  const block = await provider.getBlock(blockNumber);
+  console.log(blockNumber);
+  console.log(block);
+};
+
+process();
